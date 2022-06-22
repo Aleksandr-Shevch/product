8fa2294c7e9522b12edf368dd28d550bd8614f9d
Aleksandr-Shevch <waidyxa@gmail.com>
2022-06-22 19:07:51 +0300

    Add handdleData in ProductInfo

--- src/app/ProductInfo.java
+++ src/app/ProductInfo.java
@@ -14,6 +14,7 @@ public class ProductInfo {
 
     public static void main(String[] args) {
         doInputs();
+        showInfo(handleData());
     }
     static private void doInputs() {
         Scanner scanner = new Scanner(System.in);
@@ -24,5 +25,27 @@ public class ProductInfo {
         System.out.print("Количество товара (шт.): ");
         quantity = scanner.nextInt();
         scanner.close();
+    }
+
+    static private String handleData() {
+        product = new Product(name, weight);
+        name = product.getName();
+        totalWeight = getTotalWeight(product.getWeight(), quantity);
+        totalWeightRounded = getTotalWeightRounded(totalWeight);
+        return "------------------------\n" +
+                "Общий вес товара " + name + " (" + MEASURE + "): "
+                + totalWeightRounded;
+    }
+
+    static private double getTotalWeight(double weight, int quantity) {
+        return weight * quantity;
+    }
+
+    static private String getTotalWeightRounded(double totalWeight) {
+        return Rounder.roundWeight(totalWeight);
+    }
+
+    static private void showInfo(String output) {
+        System.out.println(output);
     }
 }
