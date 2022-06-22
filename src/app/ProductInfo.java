71a3d4b36eca975b2f35ab55b7b601eacfb834cc
Aleksandr-Shevch <waidyxa@gmail.com>
2022-06-22 19:02:31 +0300

    Add constructor in ProductInfo

--- src/app/ProductInfo.java
+++ src/app/ProductInfo.java
@@ -1,6 +1,15 @@
 package app;
 
 public class ProductInfo {
+    static String name;
+    static double weight;
+    static int quantity;
+    static Product product;
+    static double totalWeight;
+    static String totalWeightRounded;
+    static private final String MEASURE = "кг";
+
+
     public static void main(String[] args) {
         System.out.println("products");
     }
